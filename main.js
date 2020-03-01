ed9ff7223f643200d4dc863ce5196d3d305bd629
roy6307 <36528164+roy6307@users.noreply.github.com>
2020-03-01 19:12:05 +0900

    UPDATE!?!
    build command add.
    music name bug fix.
    and some new feature.

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const {app, BrowserWindow} = require('electron');
 
 /*
 2.재생도중 새 음악 재생
-3.재생 끝낼시, 삭제
 4. 재생목록
 5. 디자인
+7. 재생한거 다시 재생
+9. 캐쉬처럼 쌓아놓고 나중에 ffmpeg으로 널널할때 변환해서 용량 줄여놓기, 나중에 같은거 재생할때 용량이랑 로딩이 줄을거임.
 */
 
 function createWindow(){
